d171d95eeebbc381fd458eb2979bec766083b528
Oscar <omarquezpayall@gmail.com>
2021-09-24 14:26:41 -0400

    [ADD] Init y manifest del proyecto

---
+++ VE_Tax_Withholding/__manifest__.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'VE - Tax Withholding',
+    
+    'summary': """
+        This is a module in which you can perform tax withholdings, generate the sales and purchase ledger required by Venezuelan law. 
+    """,
+    
+    'description': """
+        This is a module in which you can perform tax withholdings, generate the sales and purchase ledger required by Venezuelan law. 
+    """,
+    
+    'author': 'Payall',
+    
+    'website': 'https://payall.com.ve/',
+    
+    'category': 'Accounting',
+    
+    'version': '1.0.0',
+    
+    'depends': ['base', 'accounting'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+    ],
+}
