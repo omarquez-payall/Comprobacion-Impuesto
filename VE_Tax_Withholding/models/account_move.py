d93527728218d3e694a2ab5fd08bfd4b06cdb30d
Oscar <omarquezpayall@gmail.com>
2021-09-28 13:35:15 -0400

    [FIX] Error de logica en programacion

--- VE_Tax_Withholding/models/account_move.py
+++ VE_Tax_Withholding/models/account_move.py
@@ -6,5 +6,6 @@ class AccountMove( models.Model):
     _inherit = 'account.move'
 
 
-    related_tax_withholding = fields.Many2one( string = 'Comprobante de Retencion',
-                                                comodel_name = 'tax.withholding_voucher')
+    related_tax_withholding = fields.One2many( string = 'Comprobante de Retencion',
+                                                comodel_name = 'tax.withholding_voucher',
+                                                inverse_name = 'related_invoice')
