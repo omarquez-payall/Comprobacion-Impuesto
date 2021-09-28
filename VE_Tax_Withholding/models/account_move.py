a7ff63266113071cbe2515d4e7718cb8f5f1557f
Oscar <omarquezpayall@gmail.com>
2021-09-28 16:01:31 -0400

    [ADD] Agregar reporte de comprobante QWEB

--- VE_Tax_Withholding/models/account_move.py
+++ VE_Tax_Withholding/models/account_move.py
@@ -6,6 +6,7 @@ class AccountMove( models.Model):
     _inherit = 'account.move'
 
 
-    related_tax_withholding = fields.One2many( string = 'Comprobante de Retencion',
-                                                comodel_name = 'tax.withholding_voucher',
-                                                inverse_name = 'related_invoice')
+    #related_tax_withholding = fields.One2many( string = 'Comprobante de Retencion',
+    #                                            comodel_name = 'tax.withholding_voucher',
+    #                                            inverse_name = 'related_invoice')
+                                                
