550812721bda77753db62b4875f860075a3a99d0
avpayall <avpayall@gmail.com>
2021-10-07 15:27:16 +0000

    [ADD] Custom salesperson module

---
+++ Custom_Salesperson/__manifest__.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+
+{
+    'name': 'Custom Salesperson',
+    
+    'summary': """
+        This is a module to include a select-inputed field to the quotations, invoices and bills. 
+    """,
+    
+    'description': """
+        This is a module to include a select-inputed field to the quotations, invoices and bills.
+    """,
+    
+    'author': 'Payall',
+    
+    'website': 'https://payall.com.ve/',
+    
+    'category': 'Sales',
+    
+    'version': '1.0.0',
+    
+    'depends': ['account_accountant','sale_management'],
+    
+    'data': [
+        
+    ],
+    
+    'demo': [
+        
+    ],
+}
