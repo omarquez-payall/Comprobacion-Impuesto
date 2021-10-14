af753ad59232c7508597f84f58760b9ab7f8952a
avpayall <avpayall@gmail.com>
2021-10-14 20:22:05 +0000

    [ADD] Form view

--- VE_Tax_Withholding/models/.ipynb_checkpoints/purchases_book-checkpoint.py
+++ VE_Tax_Withholding/models/.ipynb_checkpoints/purchases_book-checkpoint.py
@@ -6,4 +6,12 @@ class PurchasesBook( models.Model):
     _name = 'tax.purchases_book'
     _description = 'Book of every purchase bill entry by period'
     
+    name = fields.Char(string='Title', required=True)
+    
+    description = fields.Text(string='Description')
+    
+    date_from = fields.Date(string='Fecha desde')
+    
+    date_to = fields.Date(string='Fecha hasta', default=fields.Date.today)
+    
     related_invoice = fields.Many2one(string='Facturas', comodel_name='account.move', required=True)
