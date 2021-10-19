a6e6d092e904ec90914c5e658f2a28f8ead8710a
willberOdoo <willrr.payall.2021.com>
2021-10-19 15:27:27 +0000

    [fx] del modulre purchase book

--- VE_Tax_Withholding/models/purchases_book.py
+++ VE_Tax_Withholding/models/purchases_book.py
@@ -9,3 +9,10 @@ class PurchasesBook( models.Model):
     name = fields.Char(string='Title', required=True)
     
     description = fields.Text(string='Description')
+    
+    date_from = fields.Date(string='Fecha desde')
+    
+    date_to = fields.Date(string='Fecha hasta', default=fields.Date.today)
+    
+    related_invoices = fields.One2many(string='Facturas', comodel_name='account.move', inverse_name='name')
+    
